76a28b55aa767fbfffdd232d40c8d02a9015c7dd
changwoo <ep6tri@hotmail.com>
2023-05-16 03:28:34 +0900

    설정 수정

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,8 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	base: "./",
+
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
@@ -14,7 +16,10 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			fallback: 'index.html'
-		})
+		}),
+		paths: {
+			base: '/rapl-front'
+		}
 	}
 };
 
