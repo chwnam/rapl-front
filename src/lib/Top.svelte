7b074369f7801acdda59e56edf72bc5de8fbfd7c
changwoo <ep6tri@hotmail.com>
2023-05-17 02:03:01 +0900

    탑 버튼 위치 조정

--- src/lib/Top.svelte
+++ src/lib/Top.svelte
@@ -2,24 +2,38 @@
     const threshold = 250
 
     let hidden = true
+
+    let value = 0
 </script>
 
-<div class="flex justify-center items-center absolute -bottom-2 right-0 select-none back-top-top" class:hidden>
+<div id="top-button" class="flex justify-center items-center fixed bottom-[4.125rem] select-none back-top-top"
+     class:hidden>
     <a href={'#'}
        class="text-xs bg-green-300 px-2 py-1 text-black"
        on:click={() => {document.body.scrollIntoView()}}
     >TOP</a>
 </div>
 
-<svelte:window on:scroll={() => {
-    const target = document.documentElement || document.body || false
+<svelte:window
+    on:scroll={() => {
+        const target = document.documentElement || document.body || false
 
-    if (!target) {
-        return
-    }
+        if (!target) {
+            return
+        }
 
-    hidden = target.scrollTop <= threshold
-}}/>
+        hidden = target.scrollTop <= threshold
+    }}
+    on:resize={() => {
+        const button = document.getElementById('top-button'),
+            relative = button.closest('div.relative')
+
+        if (relative) {
+            const relRect = relative.getBoundingClientRect()
+            button.style.left = (relRect.right - button.clientWidth)+ 'px'
+        }
+    }}
+/>
 
 <style>
     .back-top-top {
