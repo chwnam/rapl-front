a032e92357b8cbd5573836548e536a8a5d9906a1
changwoo <ep6tri@hotmail.com>
2023-06-08 03:04:15 +0900

    검색 버그 수정

--- src/lib/Search.svelte
+++ src/lib/Search.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import {page} from '$app/stores'
     import {createParam, navigateTo} from '$lib/utils'
+
+    let search = $page.data.search
 </script>
 
 <div class="flex">
@@ -9,7 +11,7 @@
            name="search"
            type="search"
            class="box-content grow shrink min-w-0 bg-black border-4 border-solid border-green-300 p-3 placeholder:text-green-500 placeholder:italic focus:outline-none"
-           value={$page.data.search}
+           bind:value={search}
            on:keyup={(e) => {
                if ('Enter' === e.key) {
                    navigateTo(`?${createParam({search:e.target.value})}`)
@@ -21,7 +23,7 @@
             class="box-content grow-0 shrink-0 text-black bg-green-300 p-3 border-4 border-solid border-green-300"
             type="submit"
             on:click={() => {
-                navigateTo(`?${createParam({search:document.getElementById<HTMLInputElement>('search').value})}`)
+                navigateTo(`?${createParam({search})}`)
             }}
     >
         Search
