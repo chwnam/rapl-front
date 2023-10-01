f09f12c2b4a1ec33c867c31e658a15f3e086bfdb
changwoo <ep6tri@hotmail.com>
2023-10-02 00:47:43 +0900

    클립보드 복사 클릭 때 스크롤 방지

--- src/lib/CopyText.svelte
+++ src/lib/CopyText.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
-    export let text = ''
+    export let text = ""
 
-    let label = 'Copy'
+    let label = "Copy"
 
     function copyToClipboard() {
         if (text) {
             navigator.clipboard.writeText(text)
-            label = 'Copied!'
-            setTimeout(() => label = 'Copy', 10000)
+            label = "Copied!"
+            setTimeout(() => label = "Copy", 10000)
         }
     }
 </script>
 
-<a class="text-green-300" href={'#'} data-text={text} on:click={() => copyToClipboard()}>
-    {label}
-</a>
+<a class="text-green-300"
+   href={'#'}
+   data-text={text}
+   on:click={(e) => {
+        e.preventDefault()
+        copyToClipboard()
+    }}
+>{label}</a>
